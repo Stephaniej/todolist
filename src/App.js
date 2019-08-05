7331433447437653065ddc7b582a6981541cd0a7
stephaniej <stephanie.jorge@gmail.com>
2019-08-05 10:28:36 -0300

    criando lista de tarefas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from './componentes/Todo/Todo'; 
+import './estilos/global.css';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+         <Todo />
+        </div>
+    );
+  }
+}
+
+export default App;
